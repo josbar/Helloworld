f0bb2bb08cfedc381019d217c7bc455d96abc14f
josbar <aglither@gmail.com>
2018-10-21 22:57:22 +0900

    Update helloworld.php
    기본적인건 쓰고다녀라

--- helloworld.php
+++ helloworld.php
@@ -1,2 +1,12 @@
-<title>Hello, world!</title>
-<?php echo "Hello, world!\n"; ?>
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+<head>
+ <meta charset="utf-8">
+ <title>Hello, world!</title>
+</head>
+<body>
+  <?php 
+    echo "Hello, world!\n";
+  ?>
+</body>
+</html>
